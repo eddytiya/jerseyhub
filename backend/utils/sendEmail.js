02bd5883eb590fb0092fbdf604509b3a2a13d014
aditya pathak <102594671+eddytiya@users.noreply.github.com>
2026-07-16 13:23:27 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -17,29 +17,16 @@ console.log(
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // STARTTLS
-
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
-
-    tls: {
-        rejectUnauthorized: false,
-    },
-
-    // Force IPv4
-    getSocket: (options, callback) => {
-        dns.lookup(options.host, { family: 4 }, (err, address) => {
-            if (err) return callback(err);
-
-            options.host = address;
-            callback(null, false);
-        });
-    },
+    connectionTimeout: 60000,
+    greetingTimeout: 60000,
+    socketTimeout: 60000,
 });
-
 /* ==========================================
             VERIFY CONNECTION
 ========================================== */
